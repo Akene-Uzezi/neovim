1439df1e328227e4a685b0fe79d6529de63cf354
Akene-Uzezi <xtremejoexj@gmail.com>
2026-08-03 15:57:33 +0100

    changed background color to black and set theme to rose pine

---
+++ lua/plugins/black.lua
@@ -0,0 +1,10 @@
+return {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+
+      vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
+    end,
+  }
